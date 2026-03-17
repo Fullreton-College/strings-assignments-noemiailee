2e0d75797a152f5ccb723bda65eee51eb855dbfb
noemiailee <noemip0414@gmail.com>
2026-03-16 21:50:09 -0700

    Update String_Parser.cpp

--- String_Parser.cpp
+++ String_Parser.cpp
@@ -2,6 +2,36 @@
 #include <fstream>
 #include <string>
 #include <sstream> // For parsing lines
+Name: John
+Age: 23
+Initial: J
+IsStudent: true
+Grade: B
+   Name: Miguel
+Age: 22
+Initial: M
+IsStudent: true
+Grade: a
+   Name: Amos
+Age: 20
+Initial: A
+IsStudent: true
+Grade: B
+   Name: Gloria
+Age: 21
+Initial: G
+IsStudent: true
+Grade: A
+   Name: Farah
+Age: 25
+Initial: F
+IsStudent: true
+Grade: C
+   Name: Fiona
+Age: 22
+Initial: F
+IsStudent: true
+Grade: D
 using namespace std;
 
 void parseCSV(const string&);
